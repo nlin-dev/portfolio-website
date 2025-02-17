117d5677bd2432bba6935cc880c36cb15e7ce77b
Nicholas Lin <nl2872@columbia.edu>
2025-02-17 03:46:12 -0500

    Navbar to all pages and standardize

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
     console.log("Navbar loaded successfully.");
+
+    // Adding a slight boucne effect when hovering over button
+    const buttons = document.querySelectorAll(".nav-btn, .name-btn");
+
+    buttons.forEach(button => {
+        button.addEventListener("mouseenter", () => {
+            button.style.transform = "scale(1.1)";
+        });
+
+        button.addEventListener("mouseleave", () => {
+            button.style.transform = "scale(1)";
+        });
+    });
+
 });
